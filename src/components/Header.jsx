b1d2118086be1b3b3e11c5b9b2a88733a68628af
DavideCotigliani <davide.cotigliani@gmail.com>
2025-06-03 16:37:57 +0200

    [UPD] Added other css to the homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom'
 const Header = () => {
     return (
         <nav className='navbar bg-dark'>
-            <div className="container-fluid flex-column d-flex align-items-center">
-                <Link className='navbar-brand text-light mb-3' to={"/"}> Logo </Link>
-                <ul className='nav-list mb-0'>
-                    <li><Link >HomePage</Link></li>
+            <div className="container-fluid d-flex align-items-center">
+                <ul className='nav-list mb-0 d-flex align-items-center '>
+                    <li>
+                        <img className='logo-image' src="/Amazon_Prime_Video_logo_(2024).svg.png" alt="" />
+                    </li>
+                    <li><Link to={"/"} >HomePage</Link></li>
                     <li><Link >Lista dei preferiti</Link></li>
                     <li><Link >Recenti</Link></li>
                     <li><Link >Generi</Link></li>
